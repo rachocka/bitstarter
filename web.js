feb0543bf012b86e536560b07d0d04cb04a56c3f
rachocka <rachocka@gmail.com>
2013-07-08 21:04:18 +0000

    Update web.js - reading a file

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
 
+var fs = require('fs');
+var infile = "index.html"
+var buf = new Buffer('test');
+
 var app = express.createServer(express.logger());
 
+fs.readFileSync(infile, buf);
+var file_data = JSON.stringify(buf);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+    response.send(file_data);
+//	response.send('Hello');
 });
 
 var port = process.env.PORT || 5000;
